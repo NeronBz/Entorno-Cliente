6a8d78b17b93b2c7d4fe24ff5e4493ec200981d6
Raul <raulblazquezibanez@gmail.com>
2023-09-24 20:29:50 +0200

    brain training hecho

--- Práctica 6/js/script.js
+++ Práctica 6/js/script.js
@@ -1,5 +1,8 @@
 window.onload = inicio;
+
 function inicio() {
+  console.log("entro en inicio");
+
   let btn1 = document.getElementById("btn1");
   let btn2 = document.getElementById("btn2");
   let num1 = document.getElementById("num1");
@@ -9,25 +12,32 @@ function inicio() {
   let submit = document.getElementById("sub");
   let spPuntos = document.getElementById("sppuntos");
   let spFallos = document.getElementById("spFallos");
+  let Tiempo = document.getElementById("time");
 
-  let numero1 = Math.round(Math.random() * 100) + 1;
-  let numero2 = Math.round(Math.random() * 100) + 1;
-  let oper = Math.round(Math.random() * 3) + 1;
+  let numero1 = Math.round(Math.random() * 100);
+  let numero2 = Math.round(Math.random() * 100);
+  let oper = Math.round(Math.random() * 4);
+  let puntos = 0;
+  let fallos = 0;
+  let tiempo = 0;
+  let contadorSegundos = 30;
 
   const operacion = ["+", "-", "/", "*"];
 
-  let bucleActivo = true;
-
-  btn1.onclick = iniciarBucle;
-
-  function iniciarBucle() {
-    if (bucleActivo) {
-      Start();
-    }
-  }
+  btn1.onclick = Start;
 
   function Start() {
-    while (bucleActivo) {
+    console.log("entro en Start");
+    btn1.disabled = true;
+
+    tiempo = setInterval(accion, 1000);
+
+    calcular();
+
+    function calcular() {
+      numero1 = Math.round(Math.random() * 100);
+      numero2 = Math.round(Math.random() * 100);
+      oper = Math.round(Math.random() * 4);
       num1.textContent = numero1;
       op.textContent = operacion[oper];
       num2.textContent = numero2;
@@ -35,18 +45,63 @@ function inicio() {
     }
   }
 
+  function accion() {
+    if (contadorSegundos > 0) {
+      contadorSegundos--;
+      cargarInfo();
+    } else {
+      Over();
+    }
+  }
+
+  function cargarInfo() {
+    spPuntos.textContent = "Puntuación: " + puntos;
+    spFallos.textContent = "Fallos: " + fallos;
+    Tiempo.textContent = "Tiempo: " + contadorSegundos;
+  }
+
   btn2.onclick = Over;
 
   function Over() {
-    bucleActivo = false;
+    console.log("entro en Over");
+    btn1.disabled = false;
+    puntos = 0;
+    fallos = 0;
+    tiempo = 0;
+    contadorSegundos = 0;
+    cargarInfo();
+
+    clearInterval(tiempo);
   }
 
   function Comprobacion() {
-    let resultado = num1 + operacion + num2;
-    if (resultado == numero) {
-      spPuntos++;
+    console.log("entro en comprobacion");
+    let suma = numero1 + numero2;
+    let resta = numero1 - numero2;
+    let multiplicacion = num1 * numero2;
+    let division = numero1 / numero2;
+
+    let resultado = parseFloat(numero.value);
+
+    if (operacion[0] && suma == resultado) {
+      puntos++;
+      cargarInfo();
+      calcular();
+    } else if (operacion[1] && resta == resultado) {
+      puntos++;
+      cargarInfo();
+      calcular();
+    } else if (operacion[2] && division == resultado) {
+      puntos++;
+      cargarInfo();
+      calcular();
+    } else if (operacion[3] && multiplicacion == resultado) {
+      puntos++;
+      cargarInfo();
+      calcular();
     } else {
-      spFallos++;
+      fallos++;
+      cargarInfo();
     }
   }
 }
