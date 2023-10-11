732d757ab2c4bf4acd03cdaae31655ffb86871d0
RaúlInstituto <raulblazquezibanez@gmail.com>
2023-10-11 10:29:58 +0200

    miercoles(viernes)11/10

---
+++ Tema 1/DWEC_JS/EJ1_12D/ParallaxYosemite/js/script.js
@@ -0,0 +1,7 @@
+window.onload = inicio;
+
+function inicio() {
+  let imgActual = document.querySelector(".imagen3");
+  let imgDespues = document.querySelector(".imagen3");
+  imgDespues.className = ".imagen8";
+}
