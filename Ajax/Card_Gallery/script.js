d7743fda58aa8b95b1a231a57e5b5bb5febb7b70
RaúlInstituto <raulblazquezibanez@gmail.com>
2023-11-08 10:21:59 +0100

    hacer carrusel de videos, y la función eliminar

--- Ajax/Card_Gallery/script.js
+++ Ajax/Card_Gallery/script.js
@@ -9,10 +9,13 @@ function inicio() {
       //Cogemos la lista y el div contenedor del DOM
       let contenedorIndices = document.querySelector(".carousel-indicators");
       let contenedorImagenes = document.querySelector(".carousel-inner");
-      let contenedorCar = document.querySelector("#cajon");
+      let contenedorCar = document.querySelector("#cards");
       //Al hacer parse nos devuelve un objeto
       var arrayJson = JSON.parse(this.responseText);
-      var nItems = String(arrayJson).length / 4;
+      var indice = 0;
+      var caja = document.createElement("row");
+
+      let lista = document.getElementById("lista");
       arrayJson.forEach(function (fotoCamacho, posicion) {
         //PARTE 1: LISTA OL
         //Creamos el elemento li de la lista para cada imagen
@@ -24,13 +27,14 @@ function inicio() {
         if (posicion == 0) {
           elemento.className = "active";
         }
-
+        lista.appendChild(elemento);
         //Meto los li a la lista
         contenedorIndices.appendChild(elemento);
 
         //PARTE 2: DIV DE LA IMAGEN
         //Creamos el div que  va a tener las clases y la imagen
-        let caja = document.createElement("div");
+
+        caja = document.createElement("row");
 
         //Compruebo si la caja es el primer item del carousel
         if (posicion == 0) {
@@ -38,25 +42,29 @@ function inicio() {
         } else {
           caja.className = "carousel-item";
         }
-
+        //   contenedorImagenes.appendChild(caja);
         //PARTE 3: IMAGEN
         //Creamos la imagen y le damos atributos
-
-        let fila = document.createElement("row");
-
-        let imag = document.createElement("img");
-        imag.setAttribute("src", fotoCamacho.imagen);
-
-        imag.style = "width:100%;height:30vh;";
-        //crear texto para añadir nombre del empleado
-        let nombre = document.createElement("span");
-        nombre.textContent = fotoCamacho.nombre;
-
-        //Meto la imagen en la caja y la caja en el contenedor
-        caja.appendChild(imag);
-        //  caja.appendChild(nombre);
+        let columna = document.createElement("div");
+        columna.style.width = "50vw";
+        for (let i = 0; i < 4; i++) {
+          if (i + indice < arrayJson.length) {
+            var datos = arrayJson[i + indice];
+            let imagenX = document.createElement("img");
+            imagenX.style.transform = "translateX(60%)";
+            imagenX.style.margin = "2vw";
+            imagenX.setAttribute("src", datos.imagen);
+            columna.appendChild(imagenX);
+          }
+        }
+        caja.appendChild(columna);
         contenedorImagenes.appendChild(caja);
-
+        indice += 4;
+        if (indice > arrayJson.length) {
+          indice = 0;
+        }
+      });
+      arrayJson.forEach(function (fotoCamacho, posicion) {
         let columna4 = document.createElement("div");
         columna4.className = "col-lg-4";
         let equipo = document.createElement("div");
@@ -72,15 +80,16 @@ function inicio() {
         equipo_foto.appendChild(imagen);
 
         let nombreEmpleado = document.createElement("h3");
-        nombreEmpleado.innerHTML = fotoCamacho.nombre;
+        nombreEmpleado.textContent = fotoCamacho.nombre;
         equipo.appendChild(nombreEmpleado);
 
         let textoDireccion = document.createElement("div");
         textoDireccion.className = "equipo_texto";
         let spanDireccion = document.createElement("span");
-        spanDireccion.innerHTML = fotoCamacho.spanDireccion;
+        spanDireccion.textContent = fotoCamacho.direccion;
         textoDireccion.appendChild(spanDireccion);
-
+        equipo.appendChild(textoDireccion);
+        columna4.appendChild(equipo);
         contenedorCar.appendChild(columna4);
       });
     }
